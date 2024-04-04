500cac7edaeb00ec8410b50eb1b9e034f37a25cf
HaseefMuhammed <haseefmuhammedpc@gmail.com>
2024-04-04 18:23:40 +0530

    login

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("1");
+console.log("2");
+
+function login(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if(username == "haseef" && password == "ats"){
+        window.location.href = "home.html";
+    }else{
+        document.getElementById("msg1").innerHTML="Incorrect username or password";
+    }
+}
